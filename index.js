5578eaf6f1654997529ee9abfcc289a98ebd89d7
Yassine-Sayadi <yassine.sayadi@student.ucll.be>
2022-12-09 11:58:25 +0100

    added 204 as a acceptable response code

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const request = require('superagent');
   try {
     const res = await request.post(url).send(obj);
     console.log('Response body:', res.body);
-    if (![200, 201].includes(res.status)) {
+    if (![200, 201, 204].includes(res.status)) {
       core.setFailed(`Status code was not 200 or 201. Was: ${res.status}`);
     }
   } catch (error) {
