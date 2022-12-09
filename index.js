fa55cadec769cf93dc42ef2daf3f9da86e227025
Yassine-Sayadi <yassine.sayadi@student.ucll.be>
2022-12-09 12:09:37 +0100

    weee

--- index.js
+++ index.js
@@ -16,9 +16,6 @@ const request = require('superagent');
   try {
     const res = await request.post(url).send(obj);
     console.log('Response body:', res.body);
-    if (![200, 201, 204].includes(res.status)) {
-      core.setFailed(`Status code was not 200 or 201. Was: ${res.status}`);
-    }
   } catch (error) {
     core.setFailed(error.message);
   }
